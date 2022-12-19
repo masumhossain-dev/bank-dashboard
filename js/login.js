08ef82f19a2a77b0c9000e25dd3a69d99b0d1b4a
MasumHossain71 <masumhazi311@gmail.com>
2022-12-20 01:06:52 +0600

    add eventhandeler on submit button

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.getElementById('submit-btn').addEventListener('click', function(){
+   let emailField = document.getElementById('user-email');
+   let email = emailField.value;
+   let passField = document.getElementById('user-pass');
+   let pass = passField.value;
+})
