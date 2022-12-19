89095d3ae262d17c4d8ed97b8380e541a3ee8f40
MasumHossain71 <masumhazi311@gmail.com>
2022-12-20 01:29:57 +0600

    link another html page in submit btn

--- js/login.js
+++ js/login.js
@@ -3,4 +3,12 @@ document.getElementById('submit-btn').addEventListener('click', function(){
    let email = emailField.value;
    let passField = document.getElementById('user-pass');
    let pass = passField.value;
+
+   // WARNING: this is not a proper way to verify user info!!!
+   if(email === 'user@gmail.com' && pass === '12345'){
+      location.href = 'dashboard.html'
+   }
+   else{
+      alert('Invalid User!')
+   }
 })
