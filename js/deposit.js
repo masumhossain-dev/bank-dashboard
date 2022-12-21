dc6ec2369de993fc0f1473e3dbb17993ebda798e
MasumHossain71 <masumhazi311@gmail.com>
2022-12-21 13:03:44 +0600

    deposit sec and total balance sec make dynamic

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,20 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
-  let depositField = document.getElementById('deposit-field');
-  let depositValue = depositField.value;
+   let depositField = document.getElementById('deposit-field');
+   let depositOldValue = depositField.value;
+   let depositNewValue = parseFloat(depositOldValue);
 
-  let depositTotal = document.getElementById('deposit-total').innerText = depositValue;
+   let depositTotal = document.getElementById('deposit-total');
+   let depositTotalOld = depositTotal.innerText;   
+   let depositTotalNew = parseFloat(depositTotalOld);
+
+   let currentDepositTotal = depositNewValue + depositTotalNew;
+   depositTotal.innerText = currentDepositTotal;
+   depositField.value = '';
+
+   let totalBalanceField = document.getElementById('total-balance');
+   let totalBalanceOld = totalBalanceField.innerText;
+   let totalBalanceNew = parseFloat(totalBalanceOld);
+
+   let currentTotalBalance = totalBalanceNew + depositNewValue;
+   totalBalanceField.innerText = currentTotalBalance;
 })
